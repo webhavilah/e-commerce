a8b0047cc733fbb165a5f03c6f4945cd55406c18
webhavilah <your-email@example.com>
2025-11-27 17:12:55 +0100

    first commit

---
+++ src/salinaka_pages/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import logo from '../assets/salinaka-logo'
+
+function Navbar() {
+  return (
+    <header>
+        <nav>
+            <div className="">
+                <ul>
+                    <img src="" alt="" />
+                </ul>
+            </div>
+            <div className=""></div>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
