a3de6e52e284ff655a25bc5b1da0e04f7f6549f3
adekanyeayanfe@gmail.com <adekanyeayanfe@gmail.com>
2025-11-29 17:52:01 +0100

    featured section: added image hover transitions

--- src/salinaka_pages/Home.jsx
+++ src/salinaka_pages/Home.jsx
@@ -31,10 +31,10 @@ const Home = () => {
             {
               data.map((data, id)=>{
                 return(
-                  <div key={id} className='w-full gap-6 border border-[#E5E5E5]'>
+                  <div key={id} className='w-full gap-6 border border-[#E5E5E5] shadow-sm cursor-pointer '>
                     <div className="h-[200px] bg-[#F2F2F2] ">
 
-                    <img src={data.imgUrl} alt={data.brand + "'s image"}  />
+                    <img src={data.imgUrl} alt={data.brand + "'s image"} className=' mx-auto w-74 h-full hover:scale-110 transition-transform duration-300'  />
                     </div>
                     <div className="bg-[#F9F9F9] p-4">
                       <h2 className='font-medium text-2xl'>{data.brand}</h2>
