4e4a3c47a388613f8e35923236424e4ada4bda91
webhavilah <your-email@example.com>
2025-11-29 17:37:24 +0100

    tried to change the hero image position

--- src/salinaka_pages/Navbar.jsx
+++ src/salinaka_pages/Navbar.jsx
@@ -10,11 +10,11 @@ function Navbar() {
         <nav className='flex justify-center items-center w-[95%]'>
             <div className="w-[50%]">
                 <ul className='flex  items-center '>
-                    <Link to='/home'><img src={logo} className='h-[50px]' alt="" /></Link>
-                    <NavLink className={({isActive}) =>  `${isActive ? ' text-black' : 'text-gray-500'} text-sm font-semibold hover:bg-[#0000000e] px-5 transition py-2.5`} to='/'>Home</NavLink>
-                    <NavLink className={({isActive}) =>  `${isActive ? ' text-black' : 'text-gray-500'} text-sm font-semibold hover:bg-[#0000000e] px-5 transition py-2.5`} to='/shop'>Shop</NavLink>
-                    <NavLink className={({isActive}) =>  `${isActive ? ' text-black' : 'text-gray-500'} text-sm font-semibold hover:bg-[#0000000e] px-5 transition py-2.5`} to='/featured'>Featured</NavLink>
-                    <NavLink className={({isActive}) =>  `${isActive ? ' text-black' : 'text-gray-500'} text-sm font-semibold hover:bg-[#0000000e] px-5 transition py-2.5`}  to='/recommended'>Recommended</NavLink>
+                    <Link to='/'><img src={logo} className='h-[50px]' alt="" /></Link>
+                    <NavLink className={({isActive}) => `${isActive ? ' text-black' : 'text-gray-500'} text-sm font-semibold hover:bg-[#0000000e] px-5 transition py-2.5`} to='/shop'>Shop</NavLink>
+                    <NavLink className={({isActive}) => `${isActive ? ' text-black' : 'text-gray-500'} text-sm font-semibold hover:bg-[#0000000e] px-5 transition py-2.5`} to='/'>Home</NavLink>
+                    <NavLink className={({isActive}) => `${isActive ? ' text-black' : 'text-gray-500'} text-sm font-semibold hover:bg-[#0000000e] px-5 transition py-2.5`} to='/featured'>Featured</NavLink>
+                    <NavLink className={({isActive}) => `${isActive ? ' text-black' : 'text-gray-500'} text-sm font-semibold hover:bg-[#0000000e] px-5 transition py-2.5`} to='/recommended'>Recommended</NavLink>
                 </ul>
             </div>
             <div className="flex w-[50%] justify-between">
